55ad44df1eb9d1ba994473c863acdf76086e7352
Andrei Cirlan <andreynho2006@gmail.com>
2021-11-19 20:35:21 +0200

    variables and constants, create strings, store whole numbers, store decimal numbers

---
+++ day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,96 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Andrei"
+name = "Simona"
+name = "Timotei"
+
+let character = "Iasmina"
+
+var playerName = "Ionut"
+print(playerName)
+
+playerName = "Iustin"
+print(playerName)
+
+playerName = "Efrem"
+print(playerName)
+
+let managerName = "Popescu Ionut"
+let dogBreed = "Bufnitash"
+let meaningOfLife = "How many roads must a man walk down?"
+
+
+let actor = "Denzel Washington"
+
+let filename = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+let score = 10
+let reallyBig = 1000000
+let reallyBigBig = 1_000_0000
+let reallyBigBigBig = 1__0_00___00__00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+counter += 5
+
+counter *= 2
+print(score)
+counter -= 10
+print(score)
+counter /= 2
+print(score)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))
+
+let number2 = 0.1 + 0.2
+print(number2)
+let a = 1
+let b = 2.0
+// let c = a + b
+let c1 = a + Int(b)
+let c2 = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var name1 = "Nicolas Cage"
+name1 = "John Travolta"
+
+var name2 = "Nicolas Cage"
+// name2 = 57
+
+var rating = 5.0
+rating *= 2
