f65119c24de33c03fa416a28a97dc3333ef75e64
Andrei Cirlan <andreynho2006@gmail.com>
2021-11-20 16:29:02 +0200

    update day 2

--- day2/MyPlayground.playground/Contents.swift
+++ day2/MyPlayground.playground/Contents.swift
@@ -23,3 +23,31 @@ print(gameOver)
 
 gameOver.toggle()
 print(gameOver)
+
+// join string togheter
+
+// using + operator
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+// string interpolation
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+
+let number1 = 11
+//let missionMessage = "Apollo " + number + " landed on the moon."
+let missionMessage1 = "Apollo " + String(number1) + " landed on the moon."
+let missionMessage2 = "Apollo \(number1) landed on the moon."
+print("5 x 5 is \(5 * 5)")
