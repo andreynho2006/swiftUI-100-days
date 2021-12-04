5ca57a14216daabc5346a467e5cac8c26f9a9503
andreicirlan <andreynho2006@gmail.com>
2021-12-04 07:44:11 +0200

    add day 6 with loops, while loops, break, continue, checkpoint 3, update readme

---
+++ day5/breakContinue.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Cocoa
+// use continue when you want to skip the rest of the current loop iteration, and use break when you want to skip all remaining loop iterations.
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+
+    print("Found picture: \(filename)")
+}
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+print(multiples)
+let scores = [1, 8, 4, 3, 0, 5, 2]
+var count = 0
+for score in scores {
+    if score == 0 {
+        break
+    }
+    count += 1
+}
+print("You had \(count) scores before you got 0.")
---
+++ day6/readme.md
@@ -0,0 +1,11 @@
+# 100 days of SwiftUI - DAY 6
+
+## loops
+
+![Alt text](https://github.com/andreynho2006/swiftUI-100-days/blob/main/day6/img/img1.png)
+
+![Alt text](https://github.com/andreynho2006/swiftUI-100-days/blob/main/day6/img/img2.png)
+
+![Alt text](https://github.com/andreynho2006/swiftUI-100-days/blob/main/day6/img/img3.png)
+
+![Alt text](https://github.com/andreynho2006/swiftUI-100-days/blob/main/day6/img/img4.png)
